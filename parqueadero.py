c63bb61a3a3393c87471de2f5e353e72e14d0826
gomerPiratoba <133812375+gomerPiratoba@users.noreply.github.com>
2023-05-16 14:56:44 -0500

    parqueadero.py

--- parqueadero.py
+++ parqueadero.py
@@ -1,9 +1,47 @@
-- 👋 Hi, I’m @gomerPiratoba
-- 👀 I’m interested in ...
-- 🌱 I’m currently learning ...
-- 💞️ I’m looking to collaborate on ...
-- 📫 How to reach me ...
-
-<!---
-gomerPiratoba/gomerPiratoba is a ✨ special ✨ repository because its `README.md` (this file) appears on your GitHub profile.
-You can click the Preview link to take a look at your changes
+- from datetime import*
+nombreParqueadero="Parqueadero los Almendros"
+print (nombreParqueadero)
+servicio = "Servicio parqueo las 24 horas"
+print (servicio)
+#Variables para los precios
+tarifaHoraMoto=12000
+tarifaDiaMoto = 24000
+tarifaHoraCarro = 20000
+tarifaDiaCarro = 40000
+#Variabkes para  llevar el tiempo
+formato = '%Y-%m-%d %H:%M:%S'
+print('C para carros')
+print('M para motos')
+opcion = input('ingrese c o m según el caso:')
+if opcion=='M' or opcion=='m':
+    hora_ingresoM=input('Hora de ingreso:')
+    hora_salidaM=input('Hora de salida:')
+    tiempo_in=datetime.strptime(hora_ingresoM,formato)
+    tiempo_o=datetime.strptime(hora_salidaM,formato)
+    tiempo_p = tiempo_o - tiempo_in    
+    minutos=tiempo_p.seconds//60
+    horas=minutos//60
+    minutoF=minutos%60
+    dia = tiempo_p.days
+    if horas <=12 and dia<1:
+        valorApagar=horas*tarifaHoraMoto+minutoF*50
+        print("El valor a pagar es:", valorApagar)
+    else:
+        valorApagar=tarifaDiaMoto*dia
+        print("El valor a pagar es:", valorApagar)
+if opcion=='C' or opcion=='c':
+    hora_ingresoC=input('Hora de ingreso:')
+    hora_salidaC=input('Hora de salida:')
+    tiempo_in=datetime.strptime(hora_ingresoC,formato)
+    tiempo_o=datetime.strptime(hora_salidaC,formato)
+    tiempo_p = tiempo_o - tiempo_in    
+    minutos=tiempo_p.seconds//60
+    horas=minutos//60
+    minutoF=minutos%60
+    dia = tiempo_p.days
+    if horas <=12 and dia<1:
+        valorApagar=horas*tarifaHoraCarro+minutoF*50
+        print("El valor a pagar es:", valorApagar)
+    else:
+        valorApagar=tarifaDiaCarro*dia
+        print("El valor a pagar es:", valorApagar)
