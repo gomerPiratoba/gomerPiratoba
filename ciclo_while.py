fec71812c799e16ef60818f283cfb943a865ed04
gomerPiratoba <133812375+gomerPiratoba@users.noreply.github.com>
2023-05-16 16:05:48 -0500

    Create ciclo_while.py

---
+++ ciclo_while.py
@@ -0,0 +1,11 @@
+# Ciclo while
+Nombre_lista = [55, 'Hola', 25.2, "@"]
+k = 0
+while k <= 3:
+    print(Nombre_lista[k])
+    k = k + 1
+
+# Ciclo for
+lista = [55, 'Hola', 25.2, '@']
+for k in range(4):
+    print(lista[k])
